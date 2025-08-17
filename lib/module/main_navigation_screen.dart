69036493e28a9a59b75c09c71ceb61921594d397
dhruboroy <dhurboroy287@gmail.com>
2025-08-17 23:08:27 +0600

    chore:udpate navigation

--- lib/module/main_navigation_screen.dart
+++ lib/module/main_navigation_screen.dart
@@ -3,6 +3,7 @@ import 'package:task_force/module/reports/presentation/view/reports_view.dart';
 import 'package:task_force/module/settings/presentation/view/settings_view.dart';
 
 import '../ui/screens/transaction_screen.dart';
+import '../widget/bottom_sheet_widget.dart';
 import 'deshboard/presentation/view/dashboard_screen.dart';
 
 class MainNavigationScreen extends StatefulWidget {
@@ -27,6 +28,9 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
     return Scaffold(
       body: _pages[_currentIndex],
       bottomNavigationBar: NavigationBar(
+        indicatorShape: const CircleBorder(
+          side: BorderSide(color: Colors.transparent, width: 0),
+        ),
         selectedIndex: _currentIndex,
         onDestinationSelected: (index) {
           setState(() => _currentIndex = index);
@@ -37,10 +41,27 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
             icon: Icon(Icons.swap_horiz),
             label: 'Transactions',
           ),
+          SizedBox.shrink(
+            child: SizedBox(
+              width:
+                  40, // Adjust width to match the icon size// Adjust height to match the icon size
+            ),
+          ),
           NavigationDestination(icon: Icon(Icons.bar_chart), label: 'Reports'),
           NavigationDestination(icon: Icon(Icons.settings), label: 'Settings'),
         ],
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            isScrollControlled: true,
+            builder: (context) => const BottomSheetWidget(),
+          );
+        },
+        child: Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
---
+++ lib/widget/bottom_sheet_widget.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class BottomSheetWidget extends StatelessWidget {
+  const BottomSheetWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: 400,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Form(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 15.0, left: 15, right: 15),
+            child: SizedBox(
+              width: 300,
+              child: Column(
+                children: [
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Enter Deposit Amount',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                        borderSide: BorderSide(color: Colors.blue, width: 2.0),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                        borderSide: BorderSide(color: Colors.green, width: 2.0),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 15),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                    ),
+                    child: Text('Submit'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
